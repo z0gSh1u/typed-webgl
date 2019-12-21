81f130a2c4767a872c60fbe4235f99eacb05b74b
LongChen <longchen2018@qq.com>
2019-12-22 00:00:48 +0800

    增加了空气墙以及跳跃下蹲功能

--- src/work/4-justplay/roam.ts
+++ src/work/4-justplay/roam.ts
@@ -17,6 +17,17 @@ const ANGLE_UP_MAX = 120
 const ANGLE_DOWN_MAX = -120
 const VEC_UP_MAX = vec4(0.0, Math.sin(ANGLE_UP_MAX), Math.cos(ANGLE_UP_MAX), 1)
 const VEC_DOWN_MAX = vec4(0.0, Math.sin(ANGLE_DOWN_MAX), Math.cos(ANGLE_DOWN_MAX), 1)
+const POS_MIN = vec3(-0.9, 0, -0.9)//相机位置边界，分别为XZ坐标的最小值，第二个分量无效
+const POS_MAX = vec3(0.9, 0, 0.9)//相机位置边界，分别为XZ坐标的最大值，第二个分量无效
+const GRAVITY = -0.02 // 重力加速度
+const GETUP_SPEED = 0.1 // 起身速度
+const CEIL = 0.8 // 天花板坐标
+const FLOOR_STAND = -0.5 // 站立时地板坐标
+const FLOOR_SQUAT = -0.8 // 蹲下时地板坐标
+const JUMP_SPEED = 0.15 // 起跳速度
+let floor = FLOOR_STAND // 相机Y坐标最小值，可变以实现下蹲
+let isOnFloor = false // 是否站在地板上
+let verticalSpeed = 0 // 垂直方向速度，主要用于跳跃
 export let cameraPos = vec3(0.0, 0.0, 0.0)
 export let cameraFront = vec3(0.1, 0.0, 0.0)
 let cameraSpeed = 0.04
@@ -86,6 +97,9 @@ let isKeyDown: { [key: string]: boolean } = {
   '16'/*Shift*/: false
 }
 let listenKeyboardFPV = () => {
+  if (cameraMoveId == 0) {
+    cameraMoveId = window.setInterval(moveCamera, INTERVAL)
+  }
   isKeyDown['87'] = isKeyDown['65'] = isKeyDown['83'] = isKeyDown['68'] = isKeyDown['32'] = isKeyDown['16'] = false
   window.onkeydown = (e: KeyboardEvent) => {
     if (e && e.keyCode) {
@@ -95,9 +109,6 @@ let listenKeyboardFPV = () => {
       // if (cameraPos.some(v => Math.abs(v) >= 0.88)) {
       //   return
       // }
-      if (cameraMoveId == 0) {
-        cameraMoveId = window.setInterval(moveCamera, INTERVAL)
-      }
     }
   }
   window.onkeyup = (e: KeyboardEvent) => {
@@ -106,41 +117,73 @@ let listenKeyboardFPV = () => {
     }
   }
 }
+
 let moveCamera = () => {
   let cameraMoveSpeed = vec3(0, 0, 0)
   let frontVec = normalize(vec3(cameraFront[0], 0, cameraFront[2]), false)
   let leftVec = normalize(cross(VEC_Y, cameraFront), false)
-  let moveFlag = false
+  // let moveFlag = false
   if (isKeyDown['87'/*W*/]) {
     cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(cameraSpeed), frontVec)) as Vec3
-    moveFlag = true
+    // moveFlag = true
   }
   if (isKeyDown['83'/*S*/]) {
     cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(-cameraSpeed), frontVec)) as Vec3
-    moveFlag = true
+    // moveFlag = true
   }
   if (isKeyDown['65'/*A*/]) {
     cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(-cameraSpeed), leftVec)) as Vec3
-    moveFlag = true
+    // moveFlag = true
   }
   if (isKeyDown['68'/*D*/]) {
     cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(cameraSpeed), leftVec)) as Vec3
-    moveFlag = true
+    // moveFlag = true
   }
   if (isKeyDown['32'/*Space*/]) {
-    cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(cameraSpeed), VEC_Y)) as Vec3
-    moveFlag = true
+    if(isOnFloor) {
+      verticalSpeed = JUMP_SPEED
+    }
+    // cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(cameraSpeed), VEC_Y)) as Vec3
+    // moveFlag = true
   }
   if (isKeyDown['16'/*Shift*/]) {
-    cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(-cameraSpeed), VEC_Y)) as Vec3
-    moveFlag = true
+    floor = FLOOR_SQUAT
+    // cameraMoveSpeed = add(cameraMoveSpeed, mult(mat3(-cameraSpeed), VEC_Y)) as Vec3
+    // moveFlag = true
+  } else {
+    floor = FLOOR_STAND
   }
-  if (!moveFlag) {
-    clearInterval(cameraMoveId)
-    cameraMoveId = 0
-    return
+  // if (!moveFlag) {
+  //   clearInterval(cameraMoveId)
+  //   cameraMoveId = 0
+  //   return
+  // }
+  if(!isOnFloor) {
+    verticalSpeed += GRAVITY
   }
+  cameraMoveSpeed[1] += verticalSpeed
   cameraPos = add(cameraPos, cameraMoveSpeed) as Vec3
+  if(!isOnFloor && cameraPos[1] > CEIL) {
+    cameraPos[1] = CEIL
+    verticalSpeed = 0
+  }
+  if(!isOnFloor && cameraPos[1] <= floor) {
+    cameraPos[1] = floor
+    verticalSpeed = 0
+    isOnFloor = true
+  }
+  if(isOnFloor && cameraPos[1] < floor) {
+    cameraPos[1] = Math.min(cameraPos[1] + GETUP_SPEED, floor)
+  }
+  if(cameraPos[1] > floor) {
+    isOnFloor = false
+  }
+  //简单粗暴的水平方向空气墙实现
+  [0, 2].forEach(v => {
+    cameraPos[v] = Math.min(cameraPos[v], POS_MAX[v])
+    cameraPos[v] = Math.max(cameraPos[v], POS_MIN[v])
+  });
+
 }
 
 // ==================================
